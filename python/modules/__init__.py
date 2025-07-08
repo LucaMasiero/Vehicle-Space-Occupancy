74ccc214bac1cd82212a2cdd374a1b6ada52ba11
Luca1056 <luca15.masiero@gmail.com>
2025-07-08 19:28:27 +0200

    Remove python folder for feature extraction

--- python/modules/__init__.py
+++
@@ -1,5 +0,0 @@
-from .taillights_detection import taillights_detection
-from . license_plate_detection import license_plate_detection
-from .heic_to_jpg import check_format
-from .annotate_image import annotate_image
-from .SIFT import SIFT
